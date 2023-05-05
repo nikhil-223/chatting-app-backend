709a586c18d41e8e4732157a65284284a2d0bd1f
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-05-06 00:18:50 +0530

    Update JWTverification.js

--- middlewares/JWTverification.js
+++ middlewares/JWTverification.js
@@ -5,12 +5,12 @@ exports.jwtVerify = (req, res, next) => {
 	let token = req.headers.auth;
 	//token bearer token....
 	if (!token) {
-		return res.status(401).json("Please authanticate using a valid token");
+		return res.status(400).json("Please authanticate using a valid token");
 	}
 	try {
 		req.user = jwt.verify(token.split(" ")[1], JWT_SECRET);
 		next();
 	} catch (error) {
-		return res.status(401).json("Please authanticate using a valid token");
+		return res.status(400).json("Please authanticate using a valid token");
 	}
 };
